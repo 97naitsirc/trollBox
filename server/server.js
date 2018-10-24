7aa6fe45145e1f211127b0503a8d095c4a7b1078
monika ravi <monikar@hawsco.com>
2018-10-24 16:01:52 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,18 +17,43 @@ app.use(express.static(publicPath)); //will pull the only html file inside publi
 io.on('connection',(socket)=>{
 console.log('New User connected');
 
+ //emit from Admin to welcome new user
+
+ socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to Chat App',
+    createAt: new Date().getTime()
+});
+
+//broadcast emit from Admin to text new user joined
+
+socket.broadcast.emit('newMessage',{
+    from: 'Admin' ,
+        text: 'New User Joined!',
+        createAt: new Date().getTime()
+
+});
 
 socket.on('createMessage', (newMessage)=>{
 
     console.log('createMessage', newMessage);
 
-    //emits new message on client when a new message is created 
+       //emits/ calls  new message on client when a new message is created 
 
     io.emit('newMessage',{
         from: newMessage.from,
         text: newMessage.text,
         createAt: new Date().getTime()
     });
+
+    //message will be viewed by everyone but the sender
+
+    // socket.broadcast.emit('newMessage',{
+    //     from: newMessage.from,
+    //         text: newMessage.text,
+    //         createAt: new Date().getTime()
+
+    // });
 
 });
 
